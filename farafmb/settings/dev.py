849b0925c47c33e79891ce9b169a37ab557f8def
Aiven Timptner <aiven@timptner.de>
2023-09-22 19:32:16 +0200

    Initial commit

---
+++ farafmb/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&iuxwy4+ehf(&dw)0&jwzk6b1a$)s%%2$&q+-#_@-@joupkip4"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
